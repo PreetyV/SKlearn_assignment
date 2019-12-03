552dd5a1db003cd89c51bd72ad9ed69f1676b7fd
PreetyV <54750544+PreetyV@users.noreply.github.com>
2019-12-03 00:56:53 -0500

    Update SKLearn_assignment_part1.py

--- SKLearn_assignment_part1.py
+++ SKLearn_assignment_part1.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[46]:
-
 
 from sklearn.datasets import load_boston
 from sklearn import linear_model
@@ -12,7 +7,6 @@ import pandas as pd
 
 # # Finding most influencial feature of boston data set
 
-# In[47]:
 
 
 def most_influencial():
@@ -25,15 +19,7 @@ def most_influencial():
           .format(coef_df.loc[np.abs(coef_df['Coefficient']) == max(np.abs(coef_df['Coefficient']))].iloc[0]))
 
 
-# In[50]:
-
 
 if __name__ == '__main__':
     most_influencial()
-
-
-# In[ ]:
-
-
-
-
+    
